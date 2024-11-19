fed4f554918b91bda39b3bf504c124065896693c
Josh <josh.mayfield45@gmail.com>
2024-11-19 15:52:09 -0500

    added credits button and logic to fade in credits while fading out text and to reverse this when the close button is clicked

--- script.js
+++ script.js
@@ -32,16 +32,23 @@ const video = document.getElementById('video');
 const text = document.querySelector('.clickable-text');
 const closeButton = document.getElementById('closeButton');
 const videoSource = './NOGOODDREAM_WEB.mp4';
+const creditsBtn = document.getElementById('creditsBtn');
+const creditsContainer = document.getElementById('creditsContainer');
+const credits = document.getElementById('creditsImg');
+const creditSource = './Credits stills_5.1.1.webp';
+const closeCredits = document.getElementById('closeCreditsButton');
+
 
 text.addEventListener('click', function() {
     videoContainer.style.display = 'flex'; // Show video container
     videoContainer.style.opacity = '1'; // Fade in video
 
     setTimeout(() => {
-        video.style.opacity = 1
+        video.style.opacity = '1'
         video.src = videoSource; // Start the video
     }, 1000);
         text.classList.add('hidden'); // Fade out text
+        creditsBtn.classList.add('hidden');
 });
 
 closeButton.addEventListener('click', function() {
@@ -51,5 +58,53 @@ closeButton.addEventListener('click', function() {
     setTimeout(() => {
         videoContainer.style.display = 'none'; // Hide video container after fade out
         text.classList.remove('hidden'); // Fade text back in
+        creditsBtn.classList.remove('hidden');
 }, 1000); // Wait for the fade out transition to finish
+});
+
+// creditsBtn.addEventListener('click', function() {
+//     creditsContainer.style.display = 'flex';
+//     creditsContainer.style.opacity = '1';
+
+//     setTimeout(() => {
+//         credits.style.opacity = '1';
+//         credits.src = creditSource;
+//     }, 1000);
+//     text.classList.add('hidden');
+//     creditsBtn.classList.add('hidden');
+// });
+
+creditsBtn.addEventListener('click', function() {
+    loadingContainer.style.display = 'flex';
+    credits.style.display = 'none';
+
+    credits.style.opacity = '0';
+
+    credits.src = creditSource;
+
+    creditsContainer.style.display = 'flex';
+    creditsContainer.style.opacity = '1';
+
+    credits.onload = function() {
+        loadingContainer.style.display = 'none';
+        credits.style.display = 'flex';
+
+        setTimeout(() => {
+            credits.style.opacity = '1';
+        }, 1000);
+    };
+
+    text.classList.add('hidden');
+    creditsBtn.classList.add('hidden');
+});
+
+closeCredits.addEventListener('click', function() {
+    credits.src = '';
+    creditsContainer.style.opacity = '0';
+
+    setTimeout(() => {
+        creditsContainer.style.display = 'none';
+        text.classList.remove('hidden');
+        creditsBtn.classList.remove('hidden');
+    },1000);
 });
