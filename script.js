4496a36ca59abc5c0f762b4c070f361bfbc50bbc
Josh <josh.mayfield45@gmail.com>
2024-11-18 22:47:59 -0500

    changed function to display text and video centered in the browser

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ function showVideo() {
     const videoSource = './NOGOODDREAM_WEB.mp4';
 
     video.src = videoSource;
-    videoContainer.style.display = 'block';
+    videoContainer.style.display = 'flex';
+}
+
+function closeVideo(event) {
+    const videoContainer = document.getElementById('videoContainer');
+    const video = document.getElementById('video');
+    
+    video.src = '';
+    videoContainer.style.display = 'none';
+
+    if (event) event.stopPropagation();
 }
