e6d87e275dfa506216954c39fac5717538de275f
Josh <josh.mayfield45@gmail.com>
2024-11-27 23:38:48 -0500

    deleted unsused commented out code

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const videoContainer = document.getElementById('videoContainer');
 const video = document.getElementById('video');
-// const text = document.querySelector('.clickable-text');
 const image = document.querySelector('.clickable-image');
 const closeButton = document.getElementById('closeButton');
 const videoSource = './NOGOODDREAM_WEB.mp4';
@@ -10,29 +9,6 @@ const credits = document.getElementById('creditsImg');
 const creditSource = './Credits stills_5.1.1.webp';
 const closeCredits = document.getElementById('closeCreditsButton');
 const playPausebutton = document.getElementById('playPauseButton');
-
-
-// text.addEventListener('click', function() {
-//     text.style.opacity = '0';
-//     creditsBtn.style.opacity = '0';
-
-//     videoContainer.style.display = 'flex'; // Show video container
-//     videoContainer.style.flexDirection = 'column';
-//     // videoContainer.style.opacity = '1'; // Fade in video
-
-//     setTimeout(() => {
-//         video.style.opacity = '1'
-//         video.src = videoSource; // Start the video
-//         video.play();
-
-//         closeButton.style.opacity = '1';
-//     }, 1000);
-
-//     setTimeout(() => {
-//         text.classList.add('hidden');
-//         creditsBtn.classList.add('hidden');
-//     }, 1000);
-// });
 
 image.addEventListener('click', function() {
     image.style.opacity = '0';
@@ -55,22 +31,6 @@ image.addEventListener('click', function() {
     }, 1000);
 });
 
-// closeButton.addEventListener('click', function() {
-//     video.style.opacity = '0';
-//     closeButton.style.opacity = '0';
-
-//     setTimeout(() => {
-//         video.src = '';
-//         videoContainer.style.display = 'none'; // Hide video container after fade out
-
-//         text.style.opacity = '1';
-//         creditsBtn.style.opacity = '1';
-
-//         text.classList.remove('hidden'); // Fade text back in
-//         creditsBtn.classList.remove('hidden');
-//     }, 1000); // Wait for the fade out transition to finish
-// });
-
 closeButton.addEventListener('click', function() {
     video.style.opacity = '0';
     closeButton.style.opacity = '0';
@@ -86,26 +46,6 @@ closeButton.addEventListener('click', function() {
         creditsBtn.classList.remove('hidden');
     }, 1000);
 });
-
-// creditsBtn.addEventListener('click', function() {
-//     creditsBtn.style.opacity = '0';
-//     text.style.opacity = '0';
-
-//     setTimeout(() => {
-//         text.classList.add('hidden');
-//         creditsBtn.classList.add('hidden');
-
-//         creditsContainer.style.display = 'flex';
-
-//         credits.src = creditSource;
-
-//         setTimeout(() => {
-//             credits.style.opacity = '1';
-//             closeCredits.style.opacity = '1';
-//         }, 100);
-
-//     }, 1000);
-// });
 
 creditsBtn.addEventListener('click', function() {
     creditsBtn.style.opacity = '0';
@@ -126,21 +66,6 @@ creditsBtn.addEventListener('click', function() {
 
     }, 1000);
 });
-
-// closeCredits.addEventListener('click', function() {
-//     credits.style.opacity = '0';
-//     closeCredits.style.opacity = '0';
-
-//     setTimeout(() => {
-//         creditsContainer.style.display = 'none';
-
-//         text.classList.remove('hidden');
-//         creditsBtn.classList.remove('hidden');
-
-//         text.style.opacity = '1';
-//         creditsBtn.style.opacity = '1';
-//     },1000);
-// });
 
 closeCredits.addEventListener('click', function() {
     credits.style.opacity = '0';
